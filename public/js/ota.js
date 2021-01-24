556c621ba16d5469d2c1432c30e9e4cab338bb56
geigea84 <geigea84@gmail.com>
2021-01-24 12:32:19 -0600

    adjustments made for querySelectors on ota.js

--- public/js/ota.js
+++ public/js/ota.js
@@ -1,34 +1,13 @@
 const addBtn = document.querySelector("#add-btn");
 const subBtn = document.querySelector("#sub-btn");
 const amount = document.querySelector("#t-amount");
-const offAddBtn = document.querySelector(".off-add-btn");
-const offSubBtn = document.querySelector(".off-sub-btn");
 
 function testOnline() {
     if (navigator.onLine) {
-        //addBtn.removeAttribute("class");
-        //subBtn.removeAttribute("class");
         console.log("You're online!");
         alert("You're online!");
     }
-    //else {
-    //    addBtn.setAttribute("class", "off-add-btn");
-    //    subBtn.setAttribute("class", "off-sub-btn");
-    //}
 }
-/*
-function offlineAdd() {
-    console.log("(Offline) add $" + amount.value + " saved");
-    alert("(Offline) add $" + amount.value + " saved");
-}
-
-function offlineSub() {
-    console.log("(Offline) subtract $" + amount.value + " saved");
-    alert("(Offline) subtract $" + amount.value + " saved");
-}
-*/
-//==================================================
-//==================================================
 
 function offlineAdd() {
     if (navigator.onLine == false) {
@@ -45,6 +24,5 @@ function offlineSub() {
 }
 
 window.addEventListener("online", testOnline);
-//window.addEventListener("offline", testOnline);
-offAddBtn.addEventListener("click", offlineAdd);
-offSubBtn.addEventListener("click", offlineSub);
+addBtn.addEventListener("click", offlineAdd);
+subBtn.addEventListener("click", offlineSub);
